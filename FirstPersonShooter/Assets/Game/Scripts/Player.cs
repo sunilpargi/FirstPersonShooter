9f7394b0bd1b3cb1afc9b9a4ff4fb7e28bb7d56d
sunilpargi <49692267+sunilpargi@users.noreply.github.com>
2020-02-04 15:21:42 +0530

    Implemented Chracter Controller Movement

---
+++ FirstPersonShooter/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField] private float _speed = 3.5f;
+    [SerializeField] private float _gravity = 9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(-horizontalInput, 0, -verticalInput);
+        Vector3 velocity = direction * _speed;
+        velocity.y -= _gravity;
+        velocity = transform.transform.TransformDirection(velocity);
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
